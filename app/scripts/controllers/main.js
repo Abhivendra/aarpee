cebb5908dba09ede516c17fd312897bd819430d4
Abhivendra Singh <abhivendra@Abhivendras-MacBook-Pro.local>
2016-11-05 00:40:01 +0530

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name aarpeeAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the aarpeeAppApp
+ */
+angular.module('aarpeeAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
